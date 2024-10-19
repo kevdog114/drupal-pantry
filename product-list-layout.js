7641519e74bf543f0fcaa657cfeb8b7ccb8c12f7
Kevin Schaefer <kevin.l.schaefer1394@gmail.com>
2024-10-18 23:23:56 -0500

    fixup! product list layout

--- product-list-layout.js
+++ product-list-layout.js
@@ -2,7 +2,9 @@
 
 var items = document.getElementsByClassName("views-view-responsive-grid__item");
 
-items.foreach(item => {
+for(var i = 0; i < items.length; i++)
+{
+    var item = items[i];
     item.classList.add("card");
     item.getElementsByTagName("img")[0].classList.add("card-img-top");
-});
+}
