7de9af898d34195e7383a896b81a731d00e7b847
Kevin Schaefer <kevin.l.schaefer1394@gmail.com>
2024-10-19 13:09:47 -0500

    more product list layout

--- product-list-layout.js
+++ product-list-layout.js
@@ -2,11 +2,44 @@
 
 var items = document.getElementsByClassName("views-view-responsive-grid__item");
 
+/*
+<div class="card-body">
+  <h5 class="card-title">Smartfood White Cheddar</h5>
+  <h6 class="card-subtitle mb-2 text-body-secondary">Exp: 2024-10-31</h6>
+</div>
+*/
+
+var moveElement = function(containerElement, destKey, el) {
+    var dest = containerElement.getElementsByClassName(destKey);
+    if(dest.length > 0 && el != null){
+        el.remove();
+        dest[0].append(el);
+    }
+}
+
 for(var i = 0; i < items.length; i++)
 {
     var item = items[i];
     item.classList.add("card");
+    var cardBodyHtml = `
+<div class="card-body">
+  <h5 class="card-title product-title"></h5>
+  <h6 class="card-subtitle mb-2 text-body-secondary product-exp">Exp: 2024-10-31</h6>
+</div>
+    `;
+    var cardBodyElementContainer = document.createElement("html");
+    cardBodyElementContainer.innerHTML = cardBodyHtml;
+    item.append(cardBodyElementContainer.getElementsByClassName("card-body")[0]);
+
     var img = item.getElementsByTagName("img");
-    if(img.length > 0)
-        img[0].classList.add("card-img-top");
+    var title = item.getElementsByClassName("views-field-title");
+    var quantity = item.getElementsByClassName("views-field-field-unit-amount");
+    var exp = item.getElementsByClassName("views-field-field-stock-due-date");
+    
+    
+    if(img.length > 0)      img[0].classList.add("card-img-top");
+
+    if(title.length > 0)    moveElement(item, "product-title", title[0]);
+    if(quantity.length > 0) moveElement(item, "product-exp", quantity[0]);
+    if(exp.length > 0)      moveElement(item, "product-exp", exp[0]);
 }
