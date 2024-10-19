78c3207a21cf6cceab51378c9b86001e9756ece8
Kevin Schaefer <kevin.l.schaefer1394@gmail.com>
2024-10-19 13:46:47 -0500

    highlight based on exp

--- product-list-layout.js
+++ product-list-layout.js
@@ -24,7 +24,7 @@ for(var i = 0; i < items.length; i++)
     var cardBodyHtml = `
 <div class="card-body">
   <h5 class="card-title product-title"></h5>
-  <h6 class="card-subtitle mb-2 text-body-secondary product-exp">Exp: 2024-10-31</h6>
+  <h6 class="card-subtitle mb-2 text-body-secondary product-exp"></h6>
 </div>
     `;
     var cardBodyElementContainer = document.createElement("html");
@@ -36,10 +36,26 @@ for(var i = 0; i < items.length; i++)
     var quantity = item.getElementsByClassName("views-field-field-unit-amount");
     var exp = item.getElementsByClassName("views-field-field-stock-due-date");
     
-    
+
     if(img.length > 0)      img[0].classList.add("card-img-top");
 
     if(title.length > 0)    moveElement(item, "product-title", title[0]);
     if(quantity.length > 0) moveElement(item, "product-exp", quantity[0]);
     if(exp.length > 0)      moveElement(item, "product-exp", exp[0]);
+
+    if(exp.length > 0)
+    {
+        var expDate = exp[0].getElementsByTagName("time");
+        if(expDate)
+        {
+            var expDateAsDate = new Date(expDate[0].attributes.datetime.value);
+
+            var dateDiff = Math.round((expDateAsDate - new Date()) / (1000 * 60 * 60 * 24));
+            
+            if(datediff <= 0)
+                item.classList.add("text-bg-danger");
+            else if(datediff < 30)
+                item.classList.add("text-bg-warning");
+        }
+    }
 }
