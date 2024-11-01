a214394e041eb4905a638ba18b4d4b39eb9b111e
Kevin Schaefer <kevin.l.schaefer1394@gmail.com>
2024-11-01 10:29:21 -0500

    fixup! Use class for buttons

---
+++ button.js
@@ -0,0 +1,79 @@
+
+export class Button
+{
+    /**
+     * @type { HTMLElement }
+     */
+    Element
+
+    #label = "";
+
+    get label() {
+        return this.#label;
+    }
+
+    set label(a) {
+        this.#label = a;
+        this.Element.innerText = a;
+    }
+
+    SetSpinner = function(isVisible) {
+        var spinner = this.Element.getElementsByClassName("spinner-border");
+
+        if(!isVisible && spinner.length > 0)
+          spinner.remove();
+      
+        if(spinner.length == 0 && isVisible == true)
+        {
+          spinner = document.createElement("span");
+          spinner.classList.add("spinner-border", "spinner-border-sm");
+          this.Element.insertBefore(spinner, this.Element.firstChild);
+        }
+      }
+
+    OnClick = async function() {
+        await this.#clickHandler(this);
+    }
+
+    RefreshLabel = async function() {
+        await this.#refreshLabel(this);
+    }
+
+    #refreshLabel;
+    #clickHandler;
+    /**
+     *
+     */
+    constructor(refreshLabel, clickHandler) {
+        this.#refreshLabel = refreshLabel;
+        this.#clickHandler = clickHandler;
+    }
+}
+
+/*
+
+  {
+    element: null,
+    updateLabel: () => {
+      if(!this.element) return;
+      if(currentProduct != null && currentProduct.field_shopping_list == true)
+        this.element.innerText = "Remove from shopping list";
+      else
+        this.element.innerText = "Add to shopping list";
+
+      setSpinner(this.element, currentProduct == null);
+    },
+    onClick: async () => {
+      setSpinner(this.element, true);
+      var p = await api.GetById(api.GetCurrentProductId());
+      p.field_shopping_list = !p.field_shopping_list;
+      p.UpdateProperties.field_shopping_list = true;
+      console.log("Before update", p);
+      p = await api.PatchUpdate(157, p);
+      console.log("After update", p);
+      setSpinner(this.element, false);
+      this.updateLabel();
+    }
+  }
+
+*/
