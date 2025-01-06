9f0c48be2e8deb974c8add739b81fead4b811d5d
Kevin Schaefer <kevin.l.schaefer1394@gmail.com>
2025-01-05 21:57:13 -0600

    fixup! global barcode search ability

--- site-helper.js
+++ site-helper.js
@@ -1,9 +1,10 @@
 import { searchByBarcode } from "./barcode-scanner-helper.js";
+import { Button } from "./button.js";
 
 document.addEventListener('keydown', function(event) {
     // Code to execute when a key is pressed
     console.log(event.key); // Logs the pressed key
-    if(event.key == '/' && event.target == null)
+    if(event.key == '/' && (event.target == null || event.target.tagName.toLowerCase() != 'input'))
     {
         var inp = document.createElement("input");
         inp.style.display = "none";
